7ef7af81268f9d3908bb8cf459197e3b7f47c02a
Fabian Brune <fabian.brune@klosebrothers.de>
2024-11-06 14:22:09 +0100

    add basic headline

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+const getTime = () => {
+  const dateTimeFormat = new Intl.DateTimeFormat('de-DE', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hourCycle: 'h23', // 24-Stunden-Format erzwingen
+  });
+  return dateTimeFormat.format(Date.now());
+};
+</script>
 
 <template>
-  <header>some header text</header>
+  <header>
+    <div style="display: flex; flex-direction: row; align-items: center; width: 100%; justify-content: space-between">
+      <FontAwesomeIcon icon="tasks" />
+      <h2>Daily Tasks</h2>
+      <span>Time: {{ getTime() }} </span>
+    </div>
+  </header>
 
   <main>some main content</main>
 </template>
