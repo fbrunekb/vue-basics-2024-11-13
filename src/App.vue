8cf46fa5dd7d361ce587805a2397e263a990f014
Fabian Brune <fabian.brune@klosebrothers.de>
2024-11-06 14:38:59 +0100

    update time every minute

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <script lang="ts" setup>
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { computed, onMounted, onUnmounted, ref } from 'vue';
 
-const getTime = () => {
+const time = ref(Date.now());
+
+let intervalId;
+onMounted(() => {
+  const oneMinute = 60 * 1000;
+  intervalId = setInterval(() => {
+    time.value = Date.now();
+  }, oneMinute);
+});
+
+onUnmounted(() => clearInterval(intervalId));
+
+const formattedTime = computed(() => {
   const dateTimeFormat = new Intl.DateTimeFormat('de-DE', {
     hour: '2-digit',
     minute: '2-digit',
-    second: '2-digit',
-    hourCycle: 'h23', // 24-Stunden-Format erzwingen
+    hourCycle: 'h23',
   });
-  return dateTimeFormat.format(Date.now());
-};
+  return dateTimeFormat.format(time.value);
+});
 </script>
 
 <template>
@@ -17,7 +29,7 @@ const getTime = () => {
     <div style="display: flex; flex-direction: row; align-items: center; width: 100%; justify-content: space-between">
       <FontAwesomeIcon icon="tasks" />
       <h2>Daily Tasks</h2>
-      <span>Time: {{ getTime() }} </span>
+      <span>Time: {{ formattedTime }} </span>
     </div>
   </header>
 
