930f6bc149404af67f2301e4c0224be5598e2092
Fabian Brune <fabian.brune@klosebrothers.de>
2024-11-11 15:46:44 +0100

    add delete icon

--- src/components/TodoListItem.vue
+++ src/components/TodoListItem.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 defineProps<{
   inputId: string;
   label: string;
@@ -7,8 +9,11 @@ defineProps<{
 
 <template>
   <div class="default-size-item item-container">
-    <input :id="inputId" type="checkbox" />
-    <label :for="inputId" class="item-label">{{ label }}</label>
+    <div class="labelled-checkbox">
+      <input :id="inputId" type="checkbox" />
+      <label :for="inputId" class="item-label">{{ label }}</label>
+    </div>
+    <FontAwesomeIcon class="delete-icon" icon="trash" />
   </div>
 </template>
 
@@ -18,13 +23,28 @@ defineProps<{
   align-items: center;
   background: #eeeeee;
   margin-top: 12px;
+  justify-content: space-between;
 
   &:first-child {
     margin-top: 0;
   }
 }
 
+.labelled-checkbox {
+  display: flex;
+  align-items: center;
+  padding: 4px;
+}
+
 .item-label {
   padding-left: 8px;
+}
+
+.delete-icon {
+  margin-left: 12px;
+
+  &:hover {
+    color: darkred;
+  }
 }
 </style>
